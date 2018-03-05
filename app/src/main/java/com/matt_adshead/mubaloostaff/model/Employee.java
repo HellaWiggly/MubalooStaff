09a9087d98a51ce1dab033a08e0e7c2d30825871
Matt Adshead <matt.adshead@ents24.com>
2018-03-05 11:51:50 +0000

    Add getFullName() function to Employee model.

--- app/src/main/java/com/matt_adshead/mubaloostaff/model/Employee.java
+++ app/src/main/java/com/matt_adshead/mubaloostaff/model/Employee.java
@@ -90,6 +90,10 @@ public class Employee {
         return lastName;
     }
 
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
